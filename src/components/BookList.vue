6e7fe3e953882fc60ad5036487910df78b056452
DeepSingh1062 <46215658+DeepSingh1062@users.noreply.github.com>
2021-01-16 17:31:02 +0530

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <input type="text" placeholder="Search" v-model="searchInput">
+    <input type="text" placeholder="Search Books" v-model="searchInput">
     <ul class="exists">
       <book-item v-for="book in searchedBooks" :key="book.id" :book="book"></book-item>
     </ul>
@@ -51,7 +51,7 @@ export default {
       ],
       filters:["bought","borrowed"],
       holding:"bought",
-      searchInput:""
+      searchInput: ""
     };
   },
   components: {
